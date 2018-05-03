1260bf27d916d069f970acaeb5f357186ec7675e
themelogy <ismailsagsoz@gmail.com>
2018-05-03 21:45:36 +0300

    Language updated

--- views/partials/components/language.blade.php
+++ views/partials/components/language.blade.php
@@ -1,40 +1,8 @@
 @foreach(LaravelLocalization::getSupportedLocales() as $locale => $supportedLocale)
-    @php
-        switch (Request::route()->getName()) {
-            case 'page' && isset($page):
-            $url = $page->present()->url($locale);
-            break;
-            case 'news.slug':
-            case 'blog.slug':
-            $url = $post->present()->url($locale);
-            break;
-            case 'news.category':
-            case 'blog.category':
-            case 'store.category.slug':
-            $url = $category->present()->url($locale);
-            break;
-            case 'store.product.slug':
-            $url = $product->present()->url($locale);
-            break;
-            case 'employee.view':
-            $url = $employee->present()->url($locale);
-            break;
-            case 'portfolio.slug':
-            $url = $portfolio->present()->url($locale);
-            break;
-            case 'portfolio.category.slug':
-            $url = $category->present()->url($locale);
-            break;
-            default:
-            $url = null;
-            break;
-        }
-        $localizedUrl = LaravelLocalization::getLocalizedURL($locale, $url, [], true);
-    @endphp
     @if(isset($list_type))
         <li class="{{ $locale == locale() ? 'active' : '' }}">
     @endif
-    <a rel="alternate" hreflang="{!! $locale !!}" href="{{ $localizedUrl }}" class="{{ $locale == locale() ? 'active' : '' }}">
+    <a hreflang="{!! $locale !!}" href="{{ url($locale) }}" class="{{ $locale == locale() ? 'active' : '' }}">
         {{ strtoupper($locale) }}
     </a>
     @if(isset($list_type))
